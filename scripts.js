1ad5395c7d8f4424f800ae3e242133ba5824bada
Franco Bernardele <francobernar@gmail.com>
2023-12-06 16:20:43 -0300

    cors esquivar

--- scripts.js
+++ scripts.js
@@ -1,20 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const doxeado = document.getElementById('doxeado');
+    const saludo = document.getElementById('saludo');
+    const boton = document.getElementById('boton');
 
-const doxeado=document.getElementById('doxeado');
-const saludo = document.getElementById('saludo');
-const boton = document.getElementById('boton');
-
-boton.addEventListener('click', getLocation);
-async function getLocation()
-{
-    const response = await fetch('https://ipapi.co/json/');
-    const localizacion = await response.json();
-    console.log(localizacion);
-    doxeado.innerHTML = localizacion.ip+' '+localizacion.country+' '+localizacion.city+' '+localizacion.postal;
-    const saludoResponse = await fetch('http://stefanbohacek.com/hellosalut/?cc='+localizacion.country);
-    const saludoLocalizacion = await saludoResponse.json();
-    saludo.innerHTML = saludoLocalizacion.hello;
-
-}
-})
-
+    boton.addEventListener('click', getLocation);
+    async function getLocation() {
+        const response = await fetch('https://ipapi.co/json/');
+        const localizacion = await response.json();
+        console.log(localizacion);
+        doxeado.innerHTML = localizacion.ip+' '+localizacion.country+' '+localizacion.city+' '+localizacion.postal;
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const targetUrl = 'http://stefanbohacek.com/hellosalut/?cc='+localizacion.country;
+        const saludoResponse = await fetch(proxyUrl + targetUrl);
+        const saludoLocalizacion = await saludoResponse.json();
+        saludo.innerHTML = saludoLocalizacion.hello;
+    }
+});
