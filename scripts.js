582f17e8388500bfd9b1a89bfc8296c5d61e1d92
Franco Bernardele <francobernar@gmail.com>
2023-12-06 16:12:31 -0300

    saludo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 const doxeado=document.getElementById('doxeado');
+const saludo = document.getElementById('saludo');
 const boton = document.getElementById('boton');
 
 boton.addEventListener('click', getLocation);
@@ -10,5 +11,10 @@ async function getLocation()
     const localizacion = await response.json();
     console.log(localizacion);
     doxeado.innerHTML = localizacion.ip+' '+localizacion.country+' '+localizacion.city+' '+localizacion.postal;
+    const saludoResponse = await fetch('http://stefanbohacek.com/hellosalut/?cc='+localizacion.country);
+    const saludoLocalizacion = await saludoResponse.json();
+    saludo.innerHTML = saludoLocalizacion.hello;
+
 }
 })
+
